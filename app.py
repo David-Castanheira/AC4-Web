a559e014a58b07a40b6d3a11528d5b8b5039f337
David Castanheira <david.castanheira@aluno.ifsp.edu.br>
2023-05-18 16:01:14 -0300

    Criação da template base

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__, template_folder='templates')
 
-usuarios = []
+# Adicionar chave secreta para usar nas sessões 
+
+from controllers import *
+
+# Lista de usuários usuarios = []
 
 app.run(debug=True)
